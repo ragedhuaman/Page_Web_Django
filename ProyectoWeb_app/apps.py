33aa34fa05a412e75e4fe47306012a935dbdec05
Raged Huaman <58021721+ragedhuaman@users.noreply.github.com>
2021-05-27 01:19:36 -0500

    restart

--- ProyectoWeb_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProyectowebAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ProyectoWeb_app'
--- ProyectoWeb_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from ProyectoWeb_app import views
-urlpatterns = [
-    path('home', views.home, name = 'Home'),
-    path('servicios', views.servicios, name = 'Servicios'),
-    path('tienda', views.tienda, name = 'Tienda'),
-    path('contacto', views.contacto, name = 'Contacto'),
-    path('blog', views.blog, name = 'Blog'),
-]
